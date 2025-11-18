09694523ee789c3c64e4da16c6c42fcbf2b09ef3
anaesparapani <159201364+anaesparapani@users.noreply.github.com>
2025-11-18 11:11:02 -0300

    axios-api

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.89.240.117:3000/api",
+  baseURL: "https://tcc.southafricanorth.cloudapp.azure.com:3000/api",
   headers: { accept: "application/json" },
 });
 
