10840a6652979040dc0d9f9229e2524b6c155584
anaesparapani <anaclaraesparapani@gmail.com>
2025-11-25 14:17:44 -0300

    reservasAdmin

--- src/pages/ReservasAdmin.jsx
+++ src/pages/ReservasAdmin.jsx
@@ -174,7 +174,12 @@ export default function ReservasAdmin() {
                     mb: 3,
                     borderRadius: 2,
                     bgcolor: "white",
-                    width: "23%",
+                    width: {
+                      xs: "75%", // telas muito pequenas (celular)
+                      sm: "48%", // tablets pequenos (~2 por linha)
+                      md: "31%", // telas médias (~3 por linha)
+                      lg: "23%", // desktops (~4 por linha)
+                    },
                   }}
                 >
                   <Typography
@@ -277,7 +282,7 @@ export default function ReservasAdmin() {
         open={openDialog}
         onClose={handleCloseDialog}
         onConfirm={handleDeleteReserva}
-        title="Selecione os horários que deseja excluir."
+        title="Selecione os horários que deseja deletar."
         periodos={horariosDaSala}
       />
     </Box>
